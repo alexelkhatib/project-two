2bd4c60e96fc160ce8763ab083d575a872b94012
knanfuri <knanfuri@yahoo.com>
2019-04-14 21:32:56 -0500

    product.js table in models added

---
+++ models/inventory.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {
+
+    var Inventory = sequelize.define("Inventory", {
+
+        Item_name:{
+            type:DataTypes.STRING,
+            validate:{
+                lan: [1]
+            }
+        },
+        Item_Desc:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len:[1]
+            }
+
+
+        },
+        Item_Img:{
+
+            type:DataTypes.STRING,
+            type:DataTypes.blob
+
+
+        },
+        Item_Qty:{
+
+            type:DataTypes.integer
+
+
+        },
+
+        Starting_Bid_Price:{
+
+            type:DataTypes.decimal
+        },
+        Starting_Bid_Price:{
+
+            type:DataTypes.decimal
+        },
+
+
+    });
+    return(Student)
+
+}
--- schema.sql
+++ schema.sql
@@ -1,2 +1,7 @@
-DROP DATABASE IF EXISTS 'opulent_auctions';
-CREATE DATABASE 'opulent_auctions';
+-- DROP DATABASE IF EXISTS opulent_auctions;
+CREATE DATABASE opulent_auctions;
+use  opulent_auctions;
+
+
+-- CREATE DATABASE op_auc_db;
+-- use  op_auc_db;
