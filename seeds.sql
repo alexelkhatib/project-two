3c90996dc0e5463e48e00d29dc4810378e6d1ca2
knanfuri <knanfuri@yahoo.com>
2019-04-16 10:25:47 -0500

    animal seeds added'

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO Inventory (Item_Name, Item_Desc, Item_Category,Item_Img,Item_Qty,Starting_Bid_Price, End_Bid_Price)
+	VALUES('Markhor','Mountain Goat','Animal','http://1.bp.blogspot.com/-nZ8Ej-dSi_U/Udxy7qvmXMI/AAAAAAAAAu8/hjwFHpXKAQY/s1600/Markhor.jpg,',3,5,"500000"); 
+INSERT INTO Inventory (Item_Name, Item_Desc, Item_Category,Item_Img,Item_Qty,Starting_Bid_Price, End_Bid_Price)
+	VALUES('Markhor','Mountain Goat','Animal','http://1.bp.blogspot.com/-nZ8Ej-dSi_U/Udxy7qvmXMI/AAAAAAAAAu8/hjwFHpXKAQY/s1600/Markhor.jpg,',3,5,"500000"); 
+INSERT INTO Inventory (Item_Name, Item_Desc, Item_Category,Item_Img,Item_Qty,Starting_Bid_Price, End_Bid_Price)
+	VALUES('Markhor','Mountain Goat','Animal','http://1.bp.blogspot.com/-nZ8Ej-dSi_U/Udxy7qvmXMI/AAAAAAAAAu8/hjwFHpXKAQY/s1600/Markhor.jpg,',3,5,"500000"); 
